ca73b8b93870d608388874792e40c51e1e1cfbb6
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-03-18 13:29:59 -0600

    Functional Commit 1.1

--- assets/pages/Home.jsx
+++ assets/pages/Home.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Home = () => {
+  let navigate = useNavigate();
+
   return (
     <div>
 
--- assets/pages/Posts.jsx
+++ assets/pages/Posts.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Posts = () => {
+    let navigate = useNavigate();
   return (
     <div>
         
